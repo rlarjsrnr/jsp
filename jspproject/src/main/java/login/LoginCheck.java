0d9c5545e1abb36f5d099384405a176823aff01a
kimgeonguk <mongerkg8@gmail.com>
2021-12-29 18:49:10 +0900

    12/29

--- jspproject/src/main/java/login/LoginCheck.java
+++ jspproject/src/main/java/login/LoginCheck.java
@@ -1,49 +1,48 @@
 package login;
 
 import java.io.IOException;
+
 import javax.servlet.ServletException;
-import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 
-
 public class LoginCheck extends HttpServlet {
 	private static final long serialVersionUID = 1L;
 
-	protected void processRequest(HttpServletRequest request, HttpServletResponse response) 
+	protected void processRequest(HttpServletRequest request, HttpServletResponse response)
 			throws ServletException, IOException {
-		
+
 		request.setCharacterEncoding("utf-8");
 
 		String id = request.getParameter("id");
 		String pwd = request.getParameter("pwd");
-		
-		String dbID="admin";
-		String dbPWD ="1234";
-		
-		if(dbID.equals(id)&& dbPWD.equals(pwd)) {
-			//HTTPSession 객체열기
+
+		String dbID = "admin";
+		String dbPWD = "1234";
+
+		if (dbID.equals(id) && dbPWD.equals(pwd)) {
+			// HTTPSession 객체열기
 			HttpSession session = request.getSession();
-			
-			// 클라이언트정 보를 
-			
-			
+
+			// 클라이언트정
+
 			session.setAttribute("user", id);
 		}
-		
+
 		response.sendRedirect("Login");
-		
+
 	}
 
-
-	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+	protected void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
 		processRequest(request, response);
 
 	}
 
-	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+	protected void doPost(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
 		processRequest(request, response);
 
 	}
